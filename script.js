ac17ce0912444f5097422df1bfceee267f5ef976
Miklós Joánovits <72404757+majkulmajkul@users.noreply.github.com>
2023-01-07 15:44:24 +0100

    basic functionality

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
 const sketchContainer = document.querySelector(".sketch-container");
+const body = document.querySelector("body");
 
-for (let i = 0; i < 9; i++) {
-  let child = document.createElement("div");
-  child.textContent = "x";
-  child.className = "pixel";
-  sketchContainer.appendChild(child);
+let mouseDown = false;
+body.addEventListener("mousedown", () => {
+  mouseDown = true;
+});
+body.addEventListener("mouseup", () => {
+  mouseDown = false;
+});
+
+let color = "blue";
+
+function getColor() {
+  if (color === "rgb") {
+    //to be implemented
+    return undefined;
+  } else {
+    return color;
+  }
 }
 
-const allPixels = document.querySelectorAll(".pixel");
-console.log("All pixels:", allPixels);
-
-for (let pixel of allPixels) {
-  pixel.addEventListener("click", handleClick);
+function setColorToPixel(targetPixel) {
+  if (mouseDown) {
+    targetPixel.style.background = getColor();
+  }
 }
 
-function handleClick(e) {
-  const target = e.target;
-  target.style.background = "red";
+function setupScreen(width) {
+  sketchContainer.innerHTML = "";
+  sketchContainer.style.gridTemplateColumns = "auto ".repeat(width);
+
+  for (let i = 0; i < width * width; i++) {
+    let child = document.createElement("div");
+    // child.textContent = "x";
+    child.className = "pixel";
+    sketchContainer.appendChild(child);
+  }
+
+  const allPixels = document.querySelectorAll(".pixel");
+  console.log("All pixels:", allPixels);
+
+  for (let pixel of allPixels) {
+    pixel.addEventListener("click", (e) => setColorToPixel(e.target));
+    pixel.addEventListener("mouseover", (e) => setColorToPixel(e.target));
+  }
 }
+
+setupScreen(32);
