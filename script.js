8401fd21140b6f0ec182d2aaf079c5b6d1449094
Miklós Joánovits <72404757+majkulmajkul@users.noreply.github.com>
2023-01-07 20:19:09 +0100

    Change background color working

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const sketchContainer = document.querySelector(".sketch-container");
+const hiddenDiv = document.querySelector(".hidden-div");
 const rainbowModeButton = document.querySelector("#rainbow-mode-button");
+const resetButton = document.querySelector("#reset-button");
 const primaryColorPicker = document.querySelector("#primary-color-picker");
 const backgroundColorPicker = document.querySelector(
   "#background-color-picker"
@@ -26,14 +28,29 @@ backgroundColorPicker.addEventListener("change", (e) =>
   setBackgroundColor(e.target.value)
 );
 
-let color = "black";
-let backgroundColor = "white";
+resetButton.addEventListener("click", (e) => setupScreen(width));
+
+let color = "rgb(0, 0, 0)";
+let backgroundColor = "rgb(255, 255, 255)";
+let width = 32;
 
 function setColor(newColor) {
+  console.log(newColor);
   color = newColor;
 }
 
 function setBackgroundColor(newBackgroundColor) {
+  // fuckery due to HEX -> RGB
+  hiddenDiv.style.background = newBackgroundColor;
+  newBackgroundColor = hiddenDiv.style.background;
+  //end of fuckery
+  const allPixels = document.querySelectorAll(".pixel");
+  for (const pixel of allPixels) {
+    if (pixel.style.background === backgroundColor) {
+      pixel.style.background = newBackgroundColor;
+    }
+  }
+
   backgroundColor = newBackgroundColor;
 }
 
@@ -57,17 +74,18 @@ function setColorToPixel(targetPixel) {
 }
 
 function setupScreen(width) {
+  console.log("setupScreen clicked!");
   sketchContainer.innerHTML = "";
   sketchContainer.style.gridTemplateColumns = "auto ".repeat(width);
 
   for (let i = 0; i < width * width; i++) {
     let child = document.createElement("div");
     child.className = "pixel";
+    child.style.background = backgroundColor;
     sketchContainer.appendChild(child);
   }
 
   const allPixels = document.querySelectorAll(".pixel");
-  console.log("All pixels:", allPixels);
 
   for (let pixel of allPixels) {
     pixel.addEventListener("click", (e) => setColorToPixel(e.target));
